b98b1bbf4d4c4762f09696d6d73745408959666c
bmicheal72 <132099228+bmicheal72@users.noreply.github.com>
2023-04-30 10:28:47 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleProjWarchallAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
