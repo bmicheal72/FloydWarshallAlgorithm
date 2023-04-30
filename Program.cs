866eafc4a5fa9133d42a75c94cf4b78b8277a72c
bmicheal72 <132099228+bmicheal72@users.noreply.github.com>
2023-04-30 10:39:11 -0400

    Initial commit

--- Program.cs
+++ Program.cs
@@ -2,9 +2,40 @@
 {
     internal class Program
     {
+
+        /* ------ Floyd Warshall Algorithm ------ */
+
+        public static void FloydWarshall(int[,] graph, int verticesCount)
+        {
+            int[,] distance = new int[verticesCount, verticesCount];
+
+            for(int i = 0; i < verticesCount; i++)
+            {
+                for(int j = 0; j < verticesCount; j++)
+                {
+                    distance[i, j] = graph[i, j];
+                }
+            }
+
+            for(int k = 0; k < verticesCount; k++)
+            {
+                for(int i = 0;i < verticesCount; i++)
+                {
+                    for(int j = 0;j < verticesCount; j++)
+                    {
+                        if (distance[i, k] + distance[i, k] < distance[i, j])
+                        {
+                            distance[i, j] = distance[i, k] + distance[k, j];
+                        }
+                    }
+                }
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
